113970cec36f3fe60828021cb4e7cca71c128152
Bhargavi-Murthy <115211115+Bhargavi-Murthy@users.noreply.github.com>
2023-09-28 21:04:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ img = get_img_as_base64("image.jpg")
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
-background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
 background-size: 180%;
 background-position: top left;
 background-repeat: no-repeat;
@@ -22,7 +21,7 @@ background-attachment: local;
 }}
 
 [data-testid="stSidebar"] > div:first-child {{
-background-image: url("data:image/png;base64,{img}");
+background-image: url("data:image.jpg;base64,{img}");
 background-position: center; 
 background-repeat: no-repeat;
 background-attachment: fixed;
