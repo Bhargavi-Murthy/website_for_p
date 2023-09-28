44be940eeb4e8a7a3cc03ed2532672bc21bbc534
Bhargavi-Murthy <115211115+Bhargavi-Murthy@users.noreply.github.com>
2023-09-28 15:03:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import streamlit as st
 
-# Define custom HTML and CSS
+# Custom HTML and CSS for the music recommendation system website
 custom_html = """
 <!DOCTYPE html>
 <html>
 <head>
-    <title>My Streamlit App</title>
+    <title>Music Recommendation System</title>
     <style>
         /* CSS styles go here */
 
         /* Style the body */
         body {
             font-family: Arial, sans-serif;
-            background-color: #f0f0f0;
+            background-color: #f9dada; /* Light pink background */
             margin: 0;
             padding: 0;
         }
@@ -47,15 +47,48 @@ custom_html = """
         a:hover {
             color: #00468c; /* Darker blue on hover */
         }
+
+        /* Style the music recommendation container */
+        .recommendation-container {
+            max-width: 800px;
+            margin: 20px auto;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        /* Style the recommended music list */
+        .recommended-music {
+            list-style-type: none;
+            padding: 0;
+        }
+
+        .recommended-music li {
+            margin-bottom: 10px;
+            font-weight: bold;
+        }
     </style>
 </head>
 <body>
     <header>
-        <h1>Welcome to My Streamlit App</h1>
+        <h1>Music Recommendation System</h1>
     </header>
 
-    <div class="content">
-        <p>This is a sample paragraph with <a href="#">a link</a>.</p>
+    <div class="recommendation-container">
+        <h2>Enter your mood or preference:</h2>
+        <form method="POST">
+            <input type="text" id="user_input" name="user_input" required>
+            <input type="submit" value="Recommend Music">
+        </form>
+        
+        {% if user_input %}
+            <h2>Recommended Music:</h2>
+            <ul class="recommended-music">
+                {% for song in recommended_music %}
+                    <li>{{ song }}</li>
+                {% endfor %}
+            </ul>
+        {% endif %}
     </div>
 </body>
 </html>
@@ -65,5 +98,21 @@ custom_html = """
 st.markdown(custom_html, unsafe_allow_html=True)
 
 # Streamlit content
-st.title("Streamlit Integration Example")
-st.write("You can mix custom HTML and CSS with Streamlit content.")
+st.title("Music Recommendation System")
+st.write("Enter your mood or preference, and we'll recommend some music for you!")
+
+# Dummy music recommendation function
+def recommend_music(user_input):
+    # Replace this with your music recommendation logic
+    recommended_music = ["Song 1", "Song 2", "Song 3"]
+    return recommended_music
+
+# Input for user's mood or preference
+user_input = st.text_input("Enter your mood or preference:")
+
+# Button to trigger music recommendation
+if st.button("Recommend Music"):
+    recommended_music = recommend_music(user_input)
+    st.subheader("Recommended Music:")
+    for song in recommended_music:
+        st.write(song)
