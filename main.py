65a62babeb062e3a9c3968e4eca025f13aca15b5
Bhargavi-Murthy <115211115+Bhargavi-Murthy@users.noreply.github.com>
2023-09-28 15:01:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,69 @@
 import streamlit as st
-import pandas as pd
-import base64
-import numpy as np
 
-st.title('S&P 500 App')
+# Define custom HTML and CSS
+custom_html = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Streamlit App</title>
+    <style>
+        /* CSS styles go here */
 
-st.markdown("""
-This app retrieves the list of the **S&P 500** (from Wikipedia) and its corresponding **stock closing price** (year-to-date)!
-* **Python libraries:** base64, pandas, streamlit, yfinance, numpy, matplotlib
-* **Data source:** [Wikipedia](https://en.wikipedia.org/wiki/List_of_S%26P_500_companies).
-""")
+        /* Style the body */
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+            margin: 0;
+            padding: 0;
+        }
 
-st.sidebar.header('User Input Features')
+        /* Style the header */
+        header {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 20px;
+        }
 
-# Web scraping of S&P 500 data
-#
-@st.cache
-def load_data():
-    url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
-    html = pd.read_html(url, header = 0)
-    df = html[0]
-    return df
+        /* Style the h1 header */
+        h1 {
+            color: #ff6600; /* Orange text color */
+            font-size: 36px;
+        }
 
-df = load_data()
-sector = df.groupby('GICS Sector')
+        /* Style paragraphs */
+        p {
+            font-size: 18px;
+            line-height: 1.5;
+        }
 
-# Sidebar - Sector selection
-sorted_sector_unique = sorted( df['GICS Sector'].unique() )
-selected_sector = st.sidebar.multiselect('Sector', sorted_sector_unique, sorted_sector_unique)
+        /* Style links */
+        a {
+            text-decoration: none;
+            color: #0073e6; /* Blue link color */
+        }
 
-# Filtering data
-df_selected_sector = df[ (df['GICS Sector'].isin(selected_sector)) ]
+        /* Change link color on hover */
+        a:hover {
+            color: #00468c; /* Darker blue on hover */
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Welcome to My Streamlit App</h1>
+    </header>
 
-st.header('Display Companies in Selected Sector')
-st.write('Data Dimension: ' + str(df_selected_sector.shape[0]) + ' rows and ' + str(df_selected_sector.shape[1]) + ' columns.')
-st.dataframe(df_selected_sector)
+    <div class="content">
+        <p>This is a sample paragraph with <a href="#">a link</a>.</p>
+    </div>
+</body>
+</html>
+"""
 
-# Download S&P500 data
-# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
-def filedownload(df):
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-    href = f'<a href="data:file/csv;base64,{b64}" download="SP500.csv">Download CSV File</a>'
-    return href
+# Render custom HTML in Streamlit
+st.markdown(custom_html, unsafe_allow_html=True)
 
-st.markdown(filedownload(df_selected_sector), unsafe_allow_html=True)
-
-# https://pypi.org/project/yfinance/
-
-
-
+# Streamlit content
+st.title("Streamlit Integration Example")
+st.write("You can mix custom HTML and CSS with Streamlit content.")
