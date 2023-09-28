35b9785d0a4eb8ef32e73f60b2db98684a4778c0
Bhargavi-Murthy <115211115+Bhargavi-Murthy@users.noreply.github.com>
2023-09-28 16:29:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ page = st.sidebar.radio("Go to:", ["Home", "About", "More"])
 if page == "Home":
     st.title("Music Recommendation System")
     st.write("Enter your mood or preference, and we'll recommend some music for you!")
-    
+
     # Dummy music recommendation function
     def recommend_music(user_input):
         # Replace this with your music recommendation logic
@@ -17,7 +17,7 @@ if page == "Home":
 
     # Input for user's mood or preference
     user_input = st.text_input("Enter your mood or preference:")
-    
+
     # Button to trigger music recommendation
     if st.button("Recommend Music"):
         recommended_music = recommend_music(user_input)
@@ -33,52 +33,52 @@ elif page == "More":
     st.title("More Information")
     st.write("Here you can find more information about our music recommendation system.")
 
-# Custom CSS styles
+# Custom CSS styles with background image
 st.markdown(
-    """
+    f"""
     <style>
         /* CSS styles go here */
 
         /* Style the body */
-        body {
+        body {{
             font-family: Arial, sans-serif;
-            background-image: url('https://github.com/Bhargavi-Murthy/website_for_p/blob/main/Screenshot_2019-05-08-00-18-47-705_com.whatsapp%20(1).png?raw=true'); /* Specify the image path */
+            background-image: url('background.jpg'); /* Specify the image path */
             background-size: cover; /* Cover the entire screen */
             background-repeat: no-repeat; /* No image repetition */
             margin: 0;
             padding: 0;
-        }
+        }}
 
         /* Style the header */
-        header {
-            background-color: #567;
+        header {{
+            background-color: #333;
             color: #fff;
             text-align: center;
             padding: 20px;
-        }
+        }}
 
         /* Style the h1 header */
-        h1 {
-            color: #87CEFA; /* Orange text color */
+        h1 {{
+            color: #ff6600; /* Orange text color */
             font-size: 36px;
-        }
+        }}
 
         /* Style paragraphs */
-        p {
+        p {{
             font-size: 18px;
             line-height: 1.5;
-        }
+        }}
 
         /* Style links */
-        a {
+        a {{
             text-decoration: none;
             color: #0073e6; /* Blue link color */
-        }
+        }}
 
         /* Change link color on hover */
-        a:hover {
+        a:hover {{
             color: #00468c; /* Darker blue on hover */
-        }
+        }}
     </style>
     """,
     unsafe_allow_html=True,
